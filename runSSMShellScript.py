a122f3039f18ff675ad99cc175299c2933147925
elitest <dcypherd@gmail.com>
2020-06-15 20:29:32 -0500

    Update runSSMShellScript.py
    Added auto-detection of Windows and handling as well as an semi-interactive shell

--- runSSMShellScript.py
+++ runSSMShellScript.py
@@ -7,41 +7,98 @@
 #
 
 # Wishlist:
-# Currently just runs quick commands.  Need to check on status instead.
 # Error checking?
 # output to a file?
-# interactive shell?
 
-import boto3, argparse, botocore.exceptions, time
+import boto3, argparse, botocore.exceptions, time, cmd
 
 # Sets up the argument parser
 argparser = argparse.ArgumentParser(description='Runs commands on SSM enabled hosts')
 argparser.add_argument('--profile', default='default', help='The profile name in ~/.aws/credentials')
 argparser.add_argument('--region', help='The region the instance is in.')
 argparser.add_argument('instanceid', help='The InstanceID of the host with SSM installed and configured.')
-argparser.add_argument('commandfile', help='The file with commands to be run.')
+argparser.add_argument('--commandfile', help='The file with commands to be run. If not specified a shell is emulated')
 args = argparser.parse_args()
 
+# Sets the profile for clients
 if args.profile:
     boto3.setup_default_session(profile_name=args.profile)
 
 region = args.region
 instanceid = args.instanceid
+# This starts the ssm client
 ssm_client = boto3.client('ssm', region_name=region)
 
-file = open(args.commandfile, 'r')
-script = file.read()
+# This queries SSM for information to determine Windows or Linux
+instanceInfo = ssm_client.describe_instance_information(Filters=[
+        {
+            'Key': 'InstanceIds',
+            'Values': [
+                instanceid
+            ]
+        },
+    ])
+# This should be Windows or Linux
+platform = instanceInfo['InstanceInformationList'][0]['PlatformType']
+if platform == 'Windows':
+    document = 'AWS-RunPowerShellScript'
+    prompt = 'PS AWS:\\> '
+    crlf = ''
+elif platform == 'Linux':
+    document = 'AWS-RunShellScript'
+    prompt = 'you@%s:$ ' % args.instanceid
+    crlf = ' /\n'
+else:
+	print('Unknown platform, exiting')
+	quit()
 
-response = ssm_client.send_command(
-            InstanceIds=[instanceid],
-            DocumentName='AWS-RunShellScript',
-            Parameters={'commands': [script]}, )
-command_id = response['Command']['CommandId']
+# This is the function that connects to SSM and runs the command
+def runCommand(command):
+    response = ssm_client.send_command(
+                InstanceIds=[instanceid],
+                DocumentName=document,
+                Parameters={'commands': [command]}, )
+    # Because the command takes a while to run we don't immediately receive a response
+    # the CommandID allows us to check the status  
+    command_id = response['Command']['CommandId']
+    # This is a variable for the while loop.
+    processed = 1
+    time.sleep(1)
 
-time.sleep(2)
+    # This loop checks the status and exits the loop once the status changes from Pending
+    while processed:
+        output = ssm_client.get_command_invocation(
+            CommandId=command_id,
+            InstanceId=instanceid,
+        )
+        time.sleep(1)
+        # There may be other statuses to check for. These are the ones I've encountered.
+        # There may be room here for better error checking
+        if output['StatusDetails'] == 'Pending':
+            pass
+        elif output['StatusDetails'] == 'InProgress':
+            pass
+        else:
+        	# This breaks us out of the while loop
+            processed = 0
+        
+    return(output['StandardOutputContent'])
 
-output = ssm_client.get_command_invocation(
-      CommandId=command_id,
-      InstanceId=instanceid,
-    )
-print(output['StandardOutputContent'])
+# This runs the command from a file better for long bash scripts or stagers
+if args.commandfile:
+    file = open(args.commandfile, 'r')
+    script = file.read()
+    print(runCommand(script))
+else:
+
+    class HelloWorld(cmd.Cmd):
+        intro = 'This isn\'t actually an interactive shell\n Ctrl+D to exit.'
+        prompt = prompt
+        def default(self, line):
+            print(runCommand(line+crlf))
+           #     return
+
+        def do_EOF(self, line):
+            return True
+
+    HelloWorld().cmdloop()
