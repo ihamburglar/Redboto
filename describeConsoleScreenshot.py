f3e7498066c5e343e6981dac7d22c440c0d1d13c
elitest <dcypherd@gmail.com>
2020-07-27 19:12:11 -0500

    Update describeConsoleScreenshot.py

--- describeConsoleScreenshot.py
+++ describeConsoleScreenshot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Original Author @elitest
-# This script is part of RedDolphin
-# https://github.com/elitest/RedDolphin/
+# This script is part of Redboto
+# https://github.com/elitest/Redboto/
 
 # This script uses boto3 and to take screenshots from EC2 hosts
 
